72af4d0490cf7e27a0a077312fa4005bac4b7505
NsikakApp <nsikakthompson73@gmail.com>
2017-03-02 00:03:20 +0100

    Create name

--- index.js
+++ index.js
@@ -25,7 +25,8 @@ app.get('/webhook', function (req, res) {
 app.post('/webhook', function (req, res) {  
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
-        var event = events[i];
+       // var event = events[i];
+        let event = req.body.entry[0].messaging[i]
         let sender = event.sender.id;
         if (event.message && event.message.text) {
             sendMessage(event.sender.id, {text: event.message.text + " " + sender });
