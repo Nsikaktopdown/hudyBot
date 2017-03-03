daba6ddc7c23bc224fb3a17e674a7018e66a902d
NsikakApp <nsikakthompson73@gmail.com>
2017-03-03 20:39:39 +0100

    Added quick reply

--- index.js
+++ index.js
@@ -46,23 +46,27 @@ app.post('/webhook', function (req, res) {
     });
 
 // generic function sending messages
-function sendMessage(recipientId, message) {  
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id: recipientId},
-            message: message,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
-        }
-    });
-};
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s", 
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });  
+}
 
 function receivedMessage(event) {
   var senderID = event.sender.id;
@@ -153,6 +157,6 @@ function sendHelloMessage(recipientId) {
     }
   };
 
-  sendMessage(messageData);
+  callSendAPI(messageData);
 };
 
